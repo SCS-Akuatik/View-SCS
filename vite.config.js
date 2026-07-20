7d01f50cde97a425e6de4abec88d2285cd690687
JagoAcademy <rumahbaksosidoarjo@gmail.com>
2026-07-20 14:20:41 +0700

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
@@ -8,7 +8,9 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         auth: resolve(__dirname, 'auth.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
-      },
-    },
-  },
-})
+        register: resolve(__dirname, 'register.html'),
+        eventDashboard: resolve(__dirname, 'event-dashboard.html')
+      }
+    }
+  }
+});

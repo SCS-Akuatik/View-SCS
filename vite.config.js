a1005a8e60af36471f00b9889f5639b8818161ab
JagoAcademy <academyjagorenang@gmail.com>
2026-07-29 08:14:42 +0700

    dper1

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,10 @@ export default defineConfig({
         event: resolve(__dirname, 'event.html'),     
         rank: resolve(__dirname, 'rank.html'),       
         promosi: resolve(__dirname, 'promosi.html'), 
-        f1Profile: resolve(__dirname, 'f1-profile.html'), // <-- Koma ditambahkan di sini
+        f1Profile: resolve(__dirname, 'f1-profile.html'), 
         
         // Halaman Publik (Subdomain)
-        eventPublic: resolve(__dirname, 'event-public.html')
+        eventPublic: resolve(__dirname, 'event-public.html'), // <--- KOMA DITAMBAHKAN DI SINI
         
         // --- TAMBAHKAN HALAMAN LAIN DI SINI KALAU ADA ---
         // liveResult: resolve(__dirname, 'live-result.html'),

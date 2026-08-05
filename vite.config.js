688a4c443315ddadaae3b6f71943f4bb870dffc3
JagoAcademy <academyjagorenang@gmail.com>
2026-08-05 23:52:44 +0700

    page1

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
         heatBuilder: resolve(__dirname, 'book/heat-builder.html'),
         printStartList: resolve(__dirname, 'book/print-startlist.html'),
         eventSertifikat: resolve(__dirname, 'book/event-sertifikat.html'),
-        eventSertifikat: resolve(__dirname, 'book/cetak-sertifikat.html'),
+        eventSertifikat: resolve(__dirname, 'cetak-sertifikat.html'),
       }
     }
   },
